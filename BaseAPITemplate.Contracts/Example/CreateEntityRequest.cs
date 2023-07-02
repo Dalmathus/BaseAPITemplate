09fe34f34109481d2eb0841a6f2802d886ecdab7
James Luxton <jameswluxton@gmail.com>
2023-07-02 22:09:08 +1200

    Add Contracts

---
+++ BaseAPITemplate.Contracts/Example/CreateEntityRequest.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BaseAPITemplate.Contracts.Example
+{
+    public record CreateEntityRequest(
+        string Name,
+        string Description);
+    
+}
---
+++ BaseAPITemplate.Contracts/Example/EntityResponse.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BaseAPITemplate.Contracts.Example
+{
+    public record EntityResponse(
+        string Name,
+        string Description,
+        DateTime LastModifiedDateTime);
+
+}
