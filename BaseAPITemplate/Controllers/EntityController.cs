ee0692509b1c2956879b184cca726c15af11d9a6
James Luxton <jameswluxton@gmail.com>
2023-07-02 22:02:51 +1200

    Add project files.

---
+++ BaseAPITemplate/Controllers/EntityController.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using BaseAPITemplate.Contracts.Example;
+
+namespace BaseAPITemplate.Controllers
+{
+    [ApiController]
+    public class EntityController : ControllerBase
+    {
+
+        [HttpPost("/entity")]
+        public IActionResult CreateEntity(CreateEntityRequest request)
+        {
+            return Ok(request);
+        }
+
+        [HttpGet("/entity/{id:guid}")]
+        public IActionResult GetEntity(Guid id)
+        {
+            return Ok(id);
+        }
+
+        [HttpPut("/entity/{id:guid}")]
+        public IActionResult UpsertEntity(Guid id, UpsertEntityRequest request)
+        {
+            return Ok(request);
+        }
+
+        [HttpDelete("/entity/{id:guid}")]
+        public IActionResult DeleteEntity(Guid id)
+        {
+            return Ok(id);
+        }
+    } 
+}
